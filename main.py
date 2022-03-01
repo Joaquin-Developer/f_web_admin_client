3f9ad4741668f3af2d86de318c2eca52a523c10b
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2022-03-01 18:16:14 -0300

    agrego server y modifico rutas de href en templates

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, send_from_directory
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def index():
+    return render_template("index.html")
+
+
+@app.get("/update")
+def update():
+    return render_template("update.html")
+
+
+@app.get("/create")
+def create():
+    return render_template("create.html")
+
+
+@app.route('/<path:path>', methods=["GET"])
+def static_files(path):
+    return send_from_directory('public', path)
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404errorPage.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=7000)
---
+++ run.sh
@@ -0,0 +1,4 @@
+# PATH=$(pwd)/server/main.py
+
+PATH=$(pwd)/main.py
+/usr/bin/python3 $PATH
