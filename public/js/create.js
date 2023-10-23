8a54d0e3bc1ae2ff3705809c45b8f08c3b9fa99e
Joaquin-Developer <joaquin.p.olivera@gmail.com>
2023-10-23 14:23:25 -0300

    default value in show_date

--- public/js/create.js
+++ public/js/create.js
@@ -34,3 +34,13 @@ async function insertEventHandler(event) {
 
 
 document.getElementById("btn_new_date").addEventListener("click", insertEventHandler)
+
+
+addEventListener("load", () => {
+    const date = new Date()
+    const year = date.getFullYear()
+    const month = (date.getMonth() + 1).toString().padStart(2, "0")
+    const day = date.getDate().toString().padStart(2, "0")
+
+    showDate.value = `${year}-${month}-${day}`
+})
